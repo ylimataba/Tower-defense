3cd0f85fd46f864047551f4d60005923f198a177
Pyry Viita-aho <pyryviita-aho@dhcp-87-155.eduroam.aalto.fi>
2016-11-25 15:57:36 +0200

    slow() functionality added

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -31,22 +31,34 @@ sf::Vector2f Enemy::get_position()
 void Enemy::move(float deltaTime)
 {
     float factor = deltaTime + speed;
+    slow_duration -= deltaTime;
     if(route.size() > 0)
     {
         sf::Vector2f new_position = route[0];
         sf::Vector2f delta = new_position - position;
-
+        if(this->slow_duration > 0)
+        {
+            factor = factor/2;
+        }
         if(std::abs(delta.x) >= std::abs(delta.y)){
             if( delta.x > 0.f )
+            {
                 object.move(10.f * factor, 0.f);
+            }
             else
+            {
                 object.move(-10.f * factor, 0.f);
+            }
         }
         else if(std::abs(delta.x) < std::abs(delta.y)){
             if( delta.y > 0.f )
+            {
                 object.move(0.f, 10.f * factor);
+            }
             else
+            {
                 object.move(0.f, -10.f * factor);
+            }
         }
         if( std::abs(delta.x) <= 3.f && std::abs(delta.y) <= 3.f ){
             object.setPosition(new_position);
@@ -66,13 +78,26 @@ void Enemy::move(float deltaTime)
 
 void Enemy::slow()
 {
-    this -> slow_duration = 0.5;
+    this->slow_duration = 30.f;
 }
 
 void Enemy::damage(int amount)
 {
-    this -> hp -= amount;
+    this->hp -= amount;
 }
+
+bool Enemy::is_alive()
+{
+    if(this->hp <= 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 
 /*
 basehit()
@@ -89,5 +114,11 @@ void Enemy::draw(sf::RenderTarget& rt, sf::RenderStates states) const
    rt.draw(object);
 }
 
-
+bool is_finished()
+{
+    if(route.size() == 0)
+        return true;
+    else
+        return false;
+}
 
